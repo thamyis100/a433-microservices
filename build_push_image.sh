db48c3a009ece4001ccb89db96b40755a57cd519
thamyis100 <thamyis.99@gmail.com>
2023-09-15 01:20:34 +0700

    run1

--- build_push_image.sh
+++ build_push_image.sh
@@ -10,7 +10,7 @@ docker images
 docker tag item-app:v1 ghcr.io/thamyis100/devops-proyek_1/item-app:v1
 
 # 4. Login ke GitHub Packages
-docker login --username thamyis100 --password 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 ghcr.io
+docker login --username thamyis100 --password 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 ghcr.io
 
 # 5. Mengunggah image ke GitHub Packages
-docker push ghcr.io/thamyis100/devops-proyek_1/item-app:v1
+docker push ghcr.io/thamyis100/item-app:v1
