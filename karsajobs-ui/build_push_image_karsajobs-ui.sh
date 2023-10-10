8f6c8760ab06e7865077fcc9091a93eb14335c70
thamyis100 <thamyis.99@gmail.com>
2023-10-10 21:02:29 +0700

    update3

---
+++ karsajobs-ui/build_push_image_karsajobs-ui.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# 1. Perintah untuk membuat Docker image dari Dockerfile dengan nama image item-app dan tag v1
+docker build -t karsajobsui-app:v1 .
+
+# 2. Melihat daftar image di lokal
+docker images
+
+# 3. Mengubah nama image agar sesuai dengan format GitHub Packages
+docker tag karsajobsui-app:v1 ghcr.io/thamyis100/devops-proyek_2/karsajobsui-app:v1
+
+# 4. Login ke GitHub Packages
+docker login --username thamyis100 --password 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 ghcr.io
+
+# 5. Mengunggah image ke GitHub Packages
+docker push ghcr.io/thamyis100/karsajobsui-app:v1
