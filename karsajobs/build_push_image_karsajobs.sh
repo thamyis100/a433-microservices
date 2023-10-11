3ef35e235ff3fb9d229eeaa8b3a12cda40579e9d
thamyis100 <thamyis.99@gmail.com>
2023-10-11 07:21:55 +0700

    i'm sure this is the last update

--- karsajobs/build_push_image_karsajobs.sh
+++ karsajobs/build_push_image_karsajobs.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 
 # 1. Perintah untuk membuat Docker image dari Dockerfile dengan nama image item-app dan tag v1
-docker build -t karsajobs-app:v1 .
+docker build -t karsajobs:latest .
 
 # 2. Melihat daftar image di lokal
 docker images
 
 # 3. Mengubah nama image agar sesuai dengan format GitHub Packages
-docker tag karsajobs-app:v1 ghcr.io/thamyis100/devops-proyek_2/karsajobs-app:v1
+docker tag karsajobs:latest ghcr.io/thamyis100/karsajobs:latest
 
 # 4. Login ke GitHub Packages
-docker login --username thamyis100 --password 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 ghcr.io
+docker login --username thamyis100 --password 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 ghcr.io
 
 # 5. Mengunggah image ke GitHub Packages
-docker push ghcr.io/thamyis100/devops-proyek_2/karsajobs-app:v1
+docker push ghcr.io/thamyis100/karsajobs:latest
